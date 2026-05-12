f83eddfe42607260b51617d3f99fa2f14c059195
zawaydh <zawaydhabd@gmail.com>
2026-05-12 20:18:17 +0300

    Delete lib directory

--- lib/shuffle.ts
+++
@@ -1,6 +0,0 @@
-export function shuffleOptions(options) {
-  return options
-    .map((opt) => ({ sort: Math.random(), value: opt }))
-    .sort((a, b) => a.sort - b.sort)
-    .map((a) => a.value);
-}
